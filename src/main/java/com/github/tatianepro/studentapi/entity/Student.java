018fac28fc9a70cd111b305e814572046c0bd746
Tatiane <tatiane.takeuti@outlook.com>
2021-07-04 22:35:58 -0300

    entity mapped to postgresql table

--- src/main/java/com/github/tatianepro/studentapi/entity/Student.java
+++ src/main/java/com/github/tatianepro/studentapi/entity/Student.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
 public class Student {
+    @Id
+    @SequenceGenerator(name = "student_sequence", sequenceName = "student_sequence", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
     private String name;
     private String email;
