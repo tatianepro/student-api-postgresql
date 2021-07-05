eb61af94bc2342f5ab9fa82a3097d146641d75cd
Tatiane <tatiane.takeuti@outlook.com>
2021-07-04 21:02:47 -0300

    project started

---
+++ src/main/java/com/github/tatianepro/studentapi/StudentApiApplication.java
@@ -0,0 +1,13 @@
+package com.github.tatianepro.studentapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/tatianepro/studentapi/StudentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.tatianepro.studentapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
